5042c5908f431a187ba994b7cae0083679f953c2
zbt <zbt@zabeaty-wwa.zabeaty.io>
2024-08-07 13:57:05 +0200

    czyszczenie komentarzy +obsluga bledu logowania

--- scriptLogin.js
+++ scriptLogin.js
@@ -16,28 +16,14 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     firebase.auth()
         .signInWithPopup(provider)
         .then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
             var credential = result.credential;
-
-            // This gives you a Google Access Token. You can use it to access the Google API.
             var token = credential.accessToken;
-            // The signed-in user info.
             var user = result.user;
-            // IdP data available in result.additionalUserInfo.profile.
-            // ...
-            console.log(user)
+            
             localStorage.setItem("photo", user.photoURL)
             localStorage.setItem("username", user.displayName)
             document.location.href="index.html"
         }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            console.error(errorMessage)
-
+            alert('Wystąpił nieoczekiwany błąd madafucka')
         });
 });
